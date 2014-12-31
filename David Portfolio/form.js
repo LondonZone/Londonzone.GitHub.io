167a9fc12800df6f5952912cf129f89aa8d3004e
David London <londonzone21@gmail.com>
2014-12-31 13:20:28 -0500

    initial commit

---
+++ David Portfolio/form.js
@@ -0,0 +1,7 @@
+//Parse User entry
+// .val returns the value attribute of the contact-email elements 
+var value = document.getElementByID("contact-email").value;
+
+
+// Jquery get 
+value2 = $("#contact-email").val();
---
+++ David Portfolio/js/form.js
@@ -0,0 +1,3 @@
+//Parse User entry
+var value = document.getElementByID("contact-email").value;
+// .val returns the value attribute of the contact-email elements 
