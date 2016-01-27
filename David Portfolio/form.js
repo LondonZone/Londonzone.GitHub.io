17245931d3daee9423120dccb995b68b06184f02
David London <londonzone21@gmail.com>
2016-01-27 13:05:53 -0500

    major updates, nav, about etc.

--- David Portfolio/form.js
+++
@@ -1,7 +0,0 @@
-//Parse User entry
-// .val returns the value attribute of the contact-email elements 
-var value = document.getElementByID("contact-email").value;
-
-
-// Jquery get 
-value2 = $("#contact-email").val();
